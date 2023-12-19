97ab3742d0876f34b3a9a6ae2165a853b4756f80
Armando A Joergensen <armando-a-j@hotmail.com>
2023-12-19 15:22:30 -0300

    feat: day 8

---
+++ src/app/screens/eight/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const eight: React.FC = () => {
+  return <Container />;
+};
+
+export default eight;
